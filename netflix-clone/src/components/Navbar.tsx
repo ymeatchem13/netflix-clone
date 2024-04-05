8aa72ec7dc3883cfd20497cf9ad2d4924b9f18b9
ymeatchem13 <meatchemyoshua@yahoo.com>
2024-04-04 20:53:46 -0400

    setup navbar component

--- netflix-clone/src/components/Navbar.tsx
+++ netflix-clone/src/components/Navbar.tsx
@@ -1,15 +1,18 @@
-import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faBell } from "@fortawesome/free-regular-svg-icons";
+import logo from "../assets/netflix-logo.png";
 
 const Navbar = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
+      <nav className="navbar navbar-expand-lg bg-dark">
+        <div className="container-fluid px-5">
           <a className="navbar-brand" href="#">
-            Navbar
+            <img src={logo} width={125} height={75} alt="Netflix" />
           </a>
           <button
-            className="navbar-toggler"
+            className="navbar-toggler ml-auto bg-danger"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarSupportedContent"
@@ -22,53 +25,57 @@ const Navbar = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
+                <a
+                  className="nav-link active text-white"
+                  aria-current="page"
+                  href="#"
+                >
                   Home
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Link
+                <a className="nav-link text-white" href="#">
+                  TV Shows
                 </a>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu">
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
               </li>
               <li className="nav-item">
-                <a className="nav-link disabled" aria-disabled="true">
-                  Disabled
+                <a className="nav-link text-white" href="#">
+                  Movies
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-white" href="#">
+                  New & Popular
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-white" href="#">
+                  My List
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-white" href="#">
+                  Browse By Languages
                 </a>
               </li>
             </ul>
-            <form className="d-flex" role="search">
+            <ul className="navbar-nav d-flex flex-row justify-content-between pr-3">
+              <li className="nav-item mx-3" role="button">
+                <FontAwesomeIcon
+                  icon={faSearch}
+                  size="2x"
+                  className="text-white"
+                />
+              </li>
+              <li className="nav-item mx-3" role="button">
+                <FontAwesomeIcon
+                  icon={faBell}
+                  size="2x"
+                  className="text-white"
+                />
+              </li>
+            </ul>
+            {/* <form className="d-flex" role="search">
               <input
                 className="form-control me-2"
                 type="search"
@@ -78,7 +85,7 @@ const Navbar = () => {
               <button className="btn btn-outline-success" type="submit">
                 Search
               </button>
-            </form>
+            </form> */}
           </div>
         </div>
       </nav>
